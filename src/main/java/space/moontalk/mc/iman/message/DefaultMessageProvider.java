001fd840467ec7cbb1f64b6210c0f7bb642a2338
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2022-08-06 16:45:59 +0300

    Fixed recursive placeholders

--- src/main/java/space/moontalk/mc/iman/message/DefaultMessageProvider.java
+++ src/main/java/space/moontalk/mc/iman/message/DefaultMessageProvider.java
@@ -4,6 +4,7 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.val;
 
 import space.moontalk.mc.iman.*;
 import space.moontalk.mc.iman.command.sub.ArgsRange;
@@ -21,16 +22,20 @@ public class DefaultMessageProvider implements MessageProvider, PluginHolder {
 
     @Override
     public @NonNull String makeInvalidSubcommand(@NonNull String subcommand) {
-        return getString("invalid-subcommand")
-              .replaceAll("<subcommand>", subcommand);
+        return getFormatedString(
+            "invalid-subcommand",
+            "<subcommand>", subcommand
+        );
     }
 
     @Override
     public @NonNull String makeInvalidArgsNum(@NonNull ArgsRange argsRange, int passed) {
-        return getString("invalid-args-num")
-              .replaceAll("<low>", Integer.toString(argsRange.getLow()))
-              .replaceAll("<high>", Integer.toString(argsRange.getHigh()))
-              .replaceAll("<passed>", Integer.toString(passed));
+        return getFormatedString(
+            "invalid-args-num",
+            "<low>",    Integer.toString(argsRange.getLow()),
+            "<high>",   Integer.toString(argsRange.getHigh()),
+            "<passed>", Integer.toString(passed)
+        );
     }
 
     @Override
@@ -40,8 +45,10 @@ public class DefaultMessageProvider implements MessageProvider, PluginHolder {
 
     @Override
     public @NonNull String makePlayerNotFound(@NonNull String playerName) {
-        return getString("player-not-found")
-              .replaceAll("<player>", playerName);
+        return getFormatedString(
+            "player-not-found",
+            "<player>", playerName
+        );
     }
 
 
@@ -62,8 +69,10 @@ public class DefaultMessageProvider implements MessageProvider, PluginHolder {
 
     @Override
     public @NonNull String makeMissingInventory(@NonNull String inventoryName) {
-        return getString("missing-inventory")
-              .replaceAll("<inventory>", inventoryName);
+        return getFormatedString(
+            "missing-inventory",
+            "<inventory>", inventoryName
+        );
     }
 
     @Override
@@ -73,8 +82,10 @@ public class DefaultMessageProvider implements MessageProvider, PluginHolder {
 
     @Override
     public @NonNull String makeMissingInventories(@NonNull String playerName) {
-        return getString("missing-inventories")
-              .replaceAll("<player>", playerName);
+        return getFormatedString(
+            "missing-inventories",
+            "<player>", playerName
+        );
     }
 
     @Override
@@ -84,53 +95,98 @@ public class DefaultMessageProvider implements MessageProvider, PluginHolder {
 
     @Override
     public @NonNull String makeInventories(@NonNull String playerName) {
-        return getString("inventories")
-              .replaceAll("<player>", playerName);
+        return getFormatedString(
+            "inventories",
+            "<player>", playerName
+        );
     }
 
     @Override
     public @NonNull String makeInventory(@NonNull String inventoryName) {
-        return getString("inventory")
-              .replaceAll("<inventory>", inventoryName);
+        return getFormatedString(
+            "inventory",
+            "<inventory>", inventoryName
+        );
     }
 
     @Override
     public @NonNull String makeSaveYourInventory(@NonNull String inventoryName) {
-        return getString("save-your-inventory")
-              .replaceAll("<inventory>", inventoryName);
+        return getFormatedString(
+            "save-your-inventory",
+            "<inventory>", inventoryName
+        );
     }
 
     @Override
     public @NonNull String makeSaveInventory(@NonNull String playerName, @NonNull String inventoryName) {
-        return getString("save-inventory")
-              .replaceAll("<player>", playerName)
-              .replaceAll("<inventory>", inventoryName);
+        return getFormatedString(
+            "save-inventory",
+            "<player>",    playerName,
+            "<inventory>", inventoryName
+        );
     }
 
     @Override
     public @NonNull String makeSetYourInventory(@NonNull String inventoryName) {
-        return getString("set-your-inventory")
-              .replaceAll("<inventory>", inventoryName);
+        return getFormatedString(
+            "set-your-inventory",
+            "<inventory>", inventoryName
+        );
     }
 
     @Override
     public @NonNull String makeSetInventory(@NonNull String playerName, @NonNull String inventoryName) {
-        return getString("set-inventory")
-              .replaceAll("<player>", playerName)
-              .replaceAll("<inventory>", inventoryName);
+        return getFormatedString(
+            "set-inventory",
+            "<player>",    playerName,
+            "<inventory>", inventoryName
+        );
     }
 
     @Override
     public @NonNull String makeRemoveYourInventory(@NonNull String inventoryName) {
-        return getString("remove-your-inventory")
-              .replaceAll("<inventory>", inventoryName);
+        return getFormatedString(
+            "remove-your-inventory",
+            "<inventory>", inventoryName
+        );
     }
 
     @Override
     public @NonNull String makeRemoveInventory(@NonNull String playerName, @NonNull String inventoryName) {
-        return getString("remove-inventory")
-              .replaceAll("<player>", playerName)
-              .replaceAll("<inventory>", inventoryName);
+        return getFormatedString(
+            "remove-inventory",
+            "<player>",    playerName,
+            "<inventory>", inventoryName
+        );
+    }
+
+    private @NonNull String getFormatedString(@NonNull String path, @NonNull String... replacements) {
+        return replacePlaceholders(getString(path), replacements);
+    }
+
+    private static @NonNull String replacePlaceholders(@NonNull String string, @NonNull String[] replacements) {
+        assert replacements.length % 2 == 0;
+
+        val substrings = splitByPlaceHolders(string);
+
+        replacePlaceholdersInSplited(substrings, replacements); 
+
+        return String.join("", substrings);
+    }
+
+    private static @NonNull String[] splitByPlaceHolders(@NonNull String string) {
+        return string.split("(?=<[^<>]*>)|(?<=<[^<>]*>)");   
+    }
+
+    private static void replacePlaceholdersInSplited(@NonNull String[] substrings, @NonNull String[] replacements) {
+        for (int i = 0; i < replacements.length; i += 2) {
+            val placeholder = replacements[i];
+            val changer     = replacements[i + 1]; 
+
+            for (int j = 0; j < substrings.length; ++j) 
+                if (substrings[j].equals(placeholder))
+                    substrings[j] = changer;
+        }
     }
 
     private @NonNull String getString(@NonNull String path) {
