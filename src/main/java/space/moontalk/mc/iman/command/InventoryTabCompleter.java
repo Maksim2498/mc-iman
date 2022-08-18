f042bd183817246f2d7adbedb108e0bc7982a2f2
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2022-08-18 16:16:33 +0300

    Refactoring

--- src/main/java/space/moontalk/mc/iman/command/InventoryTabCompleter.java
+++ src/main/java/space/moontalk/mc/iman/command/InventoryTabCompleter.java
@@ -1,10 +1,12 @@
 package space.moontalk.mc.iman.command;
 
-import space.moontalk.mc.commands.completion.InvalidPatternException;
+import lombok.SneakyThrows;
+
 import space.moontalk.mc.commands.completion.PatternTabCompleter;
 
 public class InventoryTabCompleter extends PatternTabCompleter {
-    public InventoryTabCompleter() throws InvalidPatternException {
+    @SneakyThrows
+    public InventoryTabCompleter() {
         super(
             "(list | ls) %p",
             "(save | set | remove | rm) %s %p"
